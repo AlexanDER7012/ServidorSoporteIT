e661b330667dd03215694682342234b7343730bd
Bryant "Jason <111195815+NET-Jason@users.noreply.github.com>
2026-05-13 23:34:27 -0600

    Tema y Host 172.20.10.8

---
+++ src/main/java/conexion/Configuracion.java
@@ -0,0 +1,9 @@
+package conexion;
+
+public class Configuracion {
+
+    public static final String HOST = "172.20.10.8";
+    public static final int PUERTO = 5000;
+
+    private Configuracion() {}
+}
